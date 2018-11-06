c7e23fd26cb89b7d546744f62ca0d671c9869681
Ganlv <ganlvtech@qq.com>
2018-11-06 16:22:08 +0800

    Use django admin collectstatic, remove static dir

--- mysite/settings.py
+++ mysite/settings.py
@@ -110,6 +110,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # djcelery
@@ -128,6 +129,7 @@ CELERYBEAT_SCHEDULER = 'djcelery.schedulers.DatabaseScheduler'
 
 
 INSTALLED_APPS += (
-    'home',
     'celery_test',
+    'home',
+    'myutils',
 )
