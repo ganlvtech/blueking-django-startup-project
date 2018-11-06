6ab994974c48d999545f5d5be6b1f8362985b341
Ganlv <ganlvtech@qq.com>
2018-11-07 01:01:05 +0800

    Add favicon

--- home/urls.py
+++ home/urls.py
@@ -1,4 +1,7 @@
+import os
+
 from django.conf.urls import url
+from django.conf.urls.static import serve
 
 from . import views
 
@@ -6,5 +9,9 @@ from . import views
 urlpatterns = [
     url(r'^files/$', views.files),
     url(r'^pyinfo/$', views.pyinfo),
+    url(r'^favicon.ico$', serve, {
+        'path': 'favicon.ico',
+        'document_root': os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static/home/images')
+    }),
     url(r'^$', views.index),
 ]
