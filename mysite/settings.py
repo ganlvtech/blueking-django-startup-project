02ef918b071c1c548326ed81ff17673e8e3c5ac1
Ganlv <ganlvtech@qq.com>
2018-11-13 04:44:26 +0800

    Fix CSP

--- mysite/settings.py
+++ mysite/settings.py
@@ -133,7 +133,7 @@ CELERYBEAT_SCHEDULER = 'djcelery.schedulers.DatabaseScheduler'
 # Content Security Policy
 # https://django-csp.readthedocs.io/en/latest/configuration.html
 CSP_FRAME_ANCESTORS = 'bk.tencent.com'
-CSP_DEFAULT_SRC = ("'self'", 'data:')
+CSP_DEFAULT_SRC = ("'self'", 'data:', 'o.qcloud.com')
 
 
 INSTALLED_APPS += (
