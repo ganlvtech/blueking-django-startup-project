80a7aec809c78bf5385ebdde32020c789bb8e198
Ganlv <ganlvtech@qq.com>
2018-11-14 14:50:07 +0800

    Add custom pyinfo.
    WAN IP Address
    MAC Address

--- myutils/utils/__init__.py
+++ myutils/utils/__init__.py
@@ -4,11 +4,25 @@ def format_time(timestamp):
 
 
 def my_pyinfo():
+    def section_custom():
+        data = []
+
+        import requests
+        ip = requests.get('http://ip-api.com/json').json()['query']
+        data.append(('WAN IP Address', ip))
+
+        from uuid import getnode as get_mac
+        mac = get_mac()
+        data.append(('MAC Address', hex(mac)))
+
+        return 'Custom Info', data
+
     from .pyinfo import section_server_info, section_system, section_py_internals, section_os_internals, section_environ, section_compression, section_ldap, section_socket, section_multimedia, section_packages
 
     data = None
     allow_import = True
     output = []
+    output.append(section_custom())
     output.append(section_server_info(data))
     output.append(section_system())
     output.append(section_py_internals())
