d69a76297f41fa077cac93de64d0861acae85ce7
Ganlv <ganlvtech@qq.com>
2018-11-21 17:28:31 +0800

    fix celery_test error when first migrate

--- celery_test/__init__.py
+++ celery_test/__init__.py
@@ -1,6 +1,10 @@
-from .models import Counter
+try:
+    from .models import Counter
 
-counter = Counter.objects.first()
-if not counter:
-    counter = Counter()
-    counter.save()
+    counter = Counter.objects.first()
+    if not counter:
+        counter = Counter()
+        counter.save()
+except:
+    pass
+
