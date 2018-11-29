7b8ca0d306bc46569dcf173d4eb8613bd9d6e6fe
Ganlv <ganlvtech@qq.com>
2018-11-30 01:36:11 +0800

    Improve go program

--- golang/go/src/main.go
+++ golang/go/src/main.go
@@ -7,9 +7,15 @@ import (
 
 func main() {
 	fmt.Println("Hello, gopher!")
-	for i := 5; i > 0; i-- {
-		fmt.Println(i)
-		time.Sleep(1 * time.Second)
+	t0 := time.Now()
+	for {
+		t := time.Now()
+		dt := t.Sub(t0)
+		fmt.Println(dt)
+		time.Sleep(50 * time.Millisecond)
+		if dt > 5 * time.Second {
+			break
+		}
 	}
 	fmt.Println("Hello, I'm back!")
 }
